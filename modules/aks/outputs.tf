a9dea64c5061c33a4bec6cf0d196a47f0f4ed0c9
krishna95sai <113511786+krishna95sai@users.noreply.github.com>
2024-03-25 13:33:08 +0530

    Update outputs.tf

--- modules/aks/outputs.tf
+++ modules/aks/outputs.tf
@@ -1,4 +1,3 @@
-```hcl
 output "kube_config" {
 value = azurerm_kubernetes_cluster.aks.kube_config_raw
 description = "Raw Kubernetes config to be used by kubectl and other compatible tools"
@@ -8,4 +7,3 @@ output "cluster_id" {
 value = azurerm_kubernetes_cluster.aks.id
 description = "The ID of the Kubernetes cluster"
 }
-```
