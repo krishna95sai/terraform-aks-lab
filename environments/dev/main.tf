c0e19dfda3765bea27c9619ec9bfd6ca4afe6d68
krishna95sai <113511786+krishna95sai@users.noreply.github.com>
2024-03-25 13:28:45 +0530

    Create main.tf

---
+++ environments/dev/main.tf
@@ -0,0 +1,17 @@
+```hcl
+module "aks" {
+source = "../../modules/aks"
+cluster_name = var.cluster_name
+location = var.location
+resource_group_name = azurerm_resource_group.rg.name
+dns_prefix = var.dns_prefix
+node_pool_name = var.node_pool_name
+node_count = var.node_count
+vm_size = var.vm_size
+}
+
+resource "azurerm_resource_group" "rg" {
+name = var.resource_group_name
+location = var.location
+}
+```
